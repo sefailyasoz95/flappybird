5202b3d710c55f1ac39d50003664b5bae97438c0
sefailyasoz95 <sefailyas1455@gmail.com>
2021-05-22 22:08:45 +0300

    f

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from "react-native-game-engine";
+import React, { useRef, useState } from "react";
+import Constants from "./Constants";
+import { StyleSheet, Text, View } from "react-native";
+import Matter from "matter-js";
+import Bird from "./Components/Bird";
+import Wall from "./Components/Wall";
+import Physics from "./Physics";
+const App = () => {
+  const engineRef = useRef();
+  const setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false }); // new instance of Matter
+    let world = engine.world;
+    let bird = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 4,
+      Constants.MAX_HEIGHT / 2,
+      50,
+      50
+    );
+    let floor = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      Constants.MAX_HEIGHT - 25,
+      Constants.MAX_WIDTH,
+      50,
+      { isStatic: true }
+    );
+    let ceiling = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      25,
+      Constants.MAX_WIDTH,
+      50,
+      { isStatic: true }
+    );
+    Matter.World.add(world, [bird, floor]);
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, size: [50, 50], color: "red", renderer: Bird },
+      floor: {
+        body: floor,
+        size: [Constants.MAX_WIDTH, 50],
+        color: "purple",
+        renderer: Wall,
+      },
+      ceiling: {
+        body: ceiling,
+        size: [Constants.MAX_WIDTH, 50],
+        color: "purple",
+        renderer: Wall,
+      },
+    };
+  };
 
-export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GameEngine
+        ref={engineRef}
+        style={styles.gameEngineStyle}
+        systems={[Physics]}
+        entities={setupWorld()}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+  gameEngineStyle: {},
 });
+
+export default App;
---
+++ Constants.js
@@ -0,0 +1,8 @@
+// Constants.js
+import { Dimensions } from "react-native";
+export default {
+  MAX_WIDTH: Dimensions.get("screen").width,
+  MAX_HEIGHT: Dimensions.get("screen").height,
+  GAP_SIZE: 200,
+  PIPE_WIDTH: 100,
+};
