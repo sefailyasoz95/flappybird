0fa2d0ac7f1672a151b23c7cf984f38d04b27fc0
sefailyasoz95 <sefailyas1455@gmail.com>
2021-05-23 11:53:43 +0300

    pipes loop added, collision needs to be checked

--- App.js
+++ App.js
@@ -6,7 +6,23 @@ import Matter from "matter-js";
 import Bird from "./Components/Bird";
 import Wall from "./Components/Wall";
 import Physics from "./Physics";
+
+const randomBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generatePipes = () => {
+  let topPipeHeight = randomBetween(75, Constants.MAX_HEIGHT / 2 - 100);
+  let bottomPipeHeight =
+    Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+  let pipes = [topPipeHeight, bottomPipeHeight];
+  if (Math.random() < 0.5) {
+    pipes = pipes.reverse();
+  }
+  return pipes;
+};
 const App = () => {
+  const [running, setRunning] = useState(true);
   const engineRef = useRef();
   const setupWorld = () => {
     let engine = Matter.Engine.create({ enableSleeping: false }); // new instance of Matter
@@ -31,7 +47,47 @@ const App = () => {
       50,
       { isStatic: true }
     );
-    Matter.World.add(world, [bird, floor]);
+    let [pipeHeightOne, pipeHeightTwo] = generatePipes();
+    let pipe1 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH - Constants.PIPE_WIDTH / 2,
+      pipeHeightOne / 2,
+      Constants.PIPE_WIDTH,
+      pipeHeightOne,
+      { isStatic: true }
+    );
+    let pipe2 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH - Constants.PIPE_WIDTH / 2,
+      Constants.MAX_HEIGHT - pipeHeightTwo / 2,
+      Constants.PIPE_WIDTH,
+      pipeHeightTwo,
+      { isStatic: true }
+    );
+
+    let [pipeHeightThree, pipeHeightFour] = generatePipes();
+    let pipe3 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH * 2 - Constants.PIPE_WIDTH / 2,
+      pipeHeightThree / 2,
+      Constants.PIPE_WIDTH,
+      pipeHeightThree,
+      { isStatic: true }
+    );
+    let pipe4 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH * 2 - Constants.PIPE_WIDTH / 2,
+      Constants.MAX_HEIGHT - pipeHeightFour / 2,
+      Constants.PIPE_WIDTH,
+      pipeHeightFour,
+      { isStatic: true }
+    );
+
+    Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
+
+    Matter.Events.on(engine, "collisionStart", (event) => {
+      let pairs = event.pairs;
+      console.log("pairs:", pairs);
+
+      engineRef.dispatch({ type: "game-over" });
+    });
+
     return {
       physics: { engine: engine, world: world },
       bird: { body: bird, size: [50, 50], color: "red", renderer: Bird },
@@ -47,15 +103,46 @@ const App = () => {
         color: "purple",
         renderer: Wall,
       },
+      pipe1: {
+        body: pipe1,
+        size: [Constants.PIPE_WIDTH, pipeHeightOne],
+        color: "green",
+        renderer: Wall,
+      },
+      pipe2: {
+        body: pipe2,
+        size: [Constants.PIPE_WIDTH, pipeHeightTwo],
+        color: "green",
+        renderer: Wall,
+      },
+      pipe3: {
+        body: pipe3,
+        size: [Constants.PIPE_WIDTH, pipeHeightThree],
+        color: "green",
+        renderer: Wall,
+      },
+      pipe4: {
+        body: pipe4,
+        size: [Constants.PIPE_WIDTH, pipeHeightFour],
+        color: "green",
+        renderer: Wall,
+      },
     };
   };
 
+  const onEvent = (e) => {
+    if (e.type === "game-over") {
+      setRunning(false);
+    }
+  };
   return (
     <View style={styles.container}>
       <GameEngine
         ref={engineRef}
         style={styles.gameEngineStyle}
         systems={[Physics]}
+        onEvent={onEvent}
+        running={running}
         entities={setupWorld()}
       />
     </View>
